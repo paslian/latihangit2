f1e5bef7d9a0073a708fa8b84021784e1e0b76cd
paslian <102953631+paslian@users.noreply.github.com>
2022-04-05 23:52:34 +0700

    second commit

---
+++ Operasi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operasi;
+
+/**
+ *
+ * @author hp
+ */
+public class Operasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println(z);
+    }
+    
+}
